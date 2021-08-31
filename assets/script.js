9c2de69688ad725f0a0d8ff2ec1c0b4fc662786a
Ruchika <ruchika0542.cse19@chitkara.edu.in>
2021-08-31 23:10:19 +0530

    31august

--- assets/script.js
+++ assets/script.js
@@ -16,9 +16,12 @@
         const cellIdx = rowIdx * 9 + colIdx + 1;
         if (board[rowIdx][colIdx] === 0) {
           document.querySelector(`#cell-${cellIdx} input`).value = "";
+          document.querySelector(`#cell-${cellIdx}`).classList.remove("placeholder")
         } else {
+          document.querySelector(`#cell-${cellIdx}`).classList.remove("placeholder")
           document.querySelector(`#cell-${cellIdx} input`).value =
             board[rowIdx][colIdx];
+          document.querySelector(`#cell-${cellIdx}`).classList.add("placeholder")
         }
       });
     });
@@ -43,9 +46,12 @@
         const cellIdx = rowIdx * 9 + colIdx + 1;
         if (board[rowIdx][colIdx] === 0) {
           document.querySelector(`#cell-${cellIdx} input`).value = "";
+          document.querySelector(`#cell-${cellIdx}`).classList.remove("placeholder")
         } else {
+          document.querySelector(`#cell-${cellIdx}`).classList.remove("placeholder")
           document.querySelector(`#cell-${cellIdx} input`).value =
             board[rowIdx][colIdx];
+            document.querySelector(`#cell-${cellIdx}`).classList.add("placeholder")
         }
       });
     });
@@ -70,24 +76,107 @@
         const cellIdx = rowIdx * 9 + colIdx + 1;
         if (board[rowIdx][colIdx] === 0) {
           document.querySelector(`#cell-${cellIdx} input`).value = "";
+          document.querySelector(`#cell-${cellIdx}`).classList.remove("placeholder")
         } else {
+          document.querySelector(`#cell-${cellIdx}`).classList.remove("placeholder")
           document.querySelector(`#cell-${cellIdx} input`).value =
             board[rowIdx][colIdx];
+            document.querySelector(`#cell-${cellIdx}`).classList.add("placeholder")
         }
       });
     });
   }
 
-    
+  /* function validateRow(rowNumber){
+    const values=[];
+    const cellStart=(rowNumber-1)*9+1;
+    const cellEnd=rowNumber*9;
+    for(let i=cellStart;i<=cellEnd;i++){
+      const v=document.querySelector(`#cell-${i} input`).value
+      if(v!=="")values.push(v)
+    }
+    let val=isSudokuArrayValid(values)
+    console.log(val)
+  }
+  const isSudokuArrayValid = (array) => {
+    const row = array.slice(0).sort().join(''),
+          passingRow = [1,2,3,4,5,6,7,8,9].join('');
+  
+    return (row === passingRow);
+  }; 
+  document.getElementById("validate").addEventListener("click", validateRow(1));  */
+   function validateRow(rowNumber){
+    const values=[];
+    const cellStart=(rowNumber-1)*9+1;
+    const cellEnd=rowNumber*9;
+    for(let i=cellStart;i<=cellEnd;i++){
+      const v=document.querySelector(`#cell-${i} input`).value
+      if(v!=="")values.push(v)
+    }
+    values.sort()
+    for(let i=1;i<9;i++){
+      if(values[i]==values[i+1]){
+        document.querySelector(`#cell-${i}`).classList.add("cell-error")
+      }
+    }
+    console.log(values);
+ 
+ 
+  }
+     
+ function validateRows(){
+  for(let i=1;i<=9;i++){
+    validateRow(i);
+  }
+ }
+   
+  function validateSudoku(){
+   return validateRow(1);
+   //return (_validate(_rows) && _validate(_cols) && _validate(_grid));
+ }
+
     window.onload=function(){
      document.getElementById("easy").addEventListener("click", setEasyBoard);
      
      document.getElementById("medium").addEventListener("click", setMediumBoard);
   
      document.getElementById("hard").addEventListener("click", setHardBoard);
+     
+     document.getElementById("validate").addEventListener("click", validateSudoku);
     }
-   
-  
-  
+    
+    /* let rows=[];
+    let columns=[];
+    let box=[];
 
+    // ROW LOOP
+ffunction isValidSudoku(board) { 
+  for (let i = 0; i < board.length; i++) { 
+    for (let j = 0; j < board.length; j++) {
+
+      let cell = board[i][j];
+
+      if(cell !== "") {
+        if (rows[i].includes(cell) {
+          return false
+        } else rows[i].push(cell);
+
+        if (columns[j].includes(cell) {
+          return false;
+        } else columns[j].push(cell);
+
+        let boxIndex = Math.floor((i / 3)) * 3 + Math.floor(j / 3);
+
+        if (boxes[boxIndex].includes(cell) {
+          return false;
+        } else boxes[boxIndex].push(cell);
+
+      }
+    }
+  } 
+
+  return true;
+
+} */
+ 
   
